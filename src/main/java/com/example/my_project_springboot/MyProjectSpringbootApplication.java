61198449037f63ebc37ddb17f17be308936bf39c
Rafael <zoletrafael101@gmail.com>
2025-09-30 15:28:46 -0300

    teste

---
+++ src/main/java/com/example/my_project_springboot/MyProjectSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_project_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProjectSpringbootApplication.class, args);
+	}
+
+}
